3283abb527902cf5351f54565d2c367e93610f9e
s0lutions <jayelliott@integrityandvalues.com>
2021-07-30 10:07:30 +1000

    TYPO - changed of to or

--- src/Event.hpp
+++ src/Event.hpp
@@ -1,7 +1,7 @@
 /*
  * Event
  *
- * An event is anything that can return true of false.
+ * An event is anything that can return true or false.
  * It is used for responder actions. It must be supertyped to the actual
  * type that has an event() method.
  * The event method is a boolean that returns either true or false
